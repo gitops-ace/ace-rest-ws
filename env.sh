b383555a0d97f9cbbedb669638a0d74c9ee913a1
Kok Sing Khong <khongks@gmail.com>
2021-11-17 18:09:30 +1100

    After cert generations

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 PREFIX=ibm
-COMMON_NAME=*.appdomain.cloud
-SAN_DNS="DNS:*.sampleingress.eu-gb.containers.appdomain.cloud,DNS:*.sampleingress.eu-gb.containers.appdomain.cloud"
+COMMON_NAME=*.au-syd.containers.appdomain.cloud
+SAN_DNS="DNS:*.au-syd.containers.appdomain.cloud"
 
 ORGANISATION=IBM
-COUNTRY=GB
-LOCALITY=London
-STATE=London
+COUNTRY=AU
+LOCALITY=Melbourne
+STATE=Victoria
 
 SOAP_SERVER_JKS_PATH=${DEPLOYMENT_ROOT}/certs/ibm-soap-server.jks
 CA_JKS_PATH=${DEPLOYMENT_ROOT}/certs/ibm-ca.jks
